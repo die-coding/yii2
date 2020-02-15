20df5de5bdcef8ff275a7e8093422f618845a3de
Sugeng Sulistiyawan <sugeng.sulistiyawan@gmail.com>
2020-02-15 11:56:06 +0700

    new structure color

--- src/helpers/Color.php
+++ src/helpers/Color.php
@@ -20,11 +20,11 @@ class Color extends BaseDiecoding
     /**
      * Material Color HEX
      *
-     * @param string $color RED - BLUEGREY
-     * @param string $shade 50 - A700
+     * @param string|null $color RED - BLUEGREY
+     * @param string|null $shade 50 - A700
      * @return void
      */
-    public static function material($color, $shade)
+    public static function material($color = null, $shade = null)
     {
         $color = strtolower($color);
         $shade = strtolower($shade);
@@ -34,6 +34,12 @@ class Color extends BaseDiecoding
             static::$_materialColor = Json::decode($json);
         }
 
-        return static::$_materialColor[$color][$shade];
+        $materialColor = static::$_materialColor;
+        if ($color && $shade)
+            $materialColor = static::$_materialColor[$color][$shade];
+        else if ($color)
+            $materialColor = static::$_materialColor[$color];
+
+        return $materialColor;
     }
 }
